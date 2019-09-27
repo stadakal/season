51a9846f7387a7e6f707bb824cda3a0aa6ad74fb
sujen <tadakaluri.sujenkumar@gmail.com>
2019-09-27 03:18:03 -0400

    Updates

---
+++ precache-manifest.8fc2e5abf83eec71154500ea31776d62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1344ad251e099d7c8c5579c7274695",
+    "url": "/season/index.html"
+  },
+  {
+    "revision": "e98eea50850d71cd50c7",
+    "url": "/season/static/css/main.aa348568.chunk.css"
+  },
+  {
+    "revision": "efed954caec3bdd89cec",
+    "url": "/season/static/js/2.e5bb1ff1.chunk.js"
+  },
+  {
+    "revision": "e98eea50850d71cd50c7",
+    "url": "/season/static/js/main.52ff4ddd.chunk.js"
+  },
+  {
+    "revision": "3d8262dbe1f757777516",
+    "url": "/season/static/js/runtime~main.47f63805.js"
+  }
+]);
